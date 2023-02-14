e79a62c7b5669cb0452bc088d3127e32cdfbb29e
Asahi Cantu <asahicantu@outlook.com>
2023-02-14 18:00:14 +0100

    Update arithmeticController.js

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,6 +11,7 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
+    //Forgot to add a comment on new changes
     'add':      function(a,b) { return a + b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
